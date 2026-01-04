64e95776922c5104d766f3759dc1012d2dee6e5b
ShrutiAgrawal129 <shru.agrawal129@gmail.com>
2026-01-04 22:10:59 +0530

    Extend SpringBootServletInitializer in main application

--- src/main/java/com/td/ToDoApplicationBootApplication.java
+++ src/main/java/com/td/ToDoApplicationBootApplication.java
@@ -2,12 +2,19 @@ package com.td;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ToDoApplicationBootApplication {
+public class ToDoApplicationBootApplication
+        extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ToDoApplicationBootApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ToDoApplicationBootApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoApplicationBootApplication.class, args);
+    }
 }
